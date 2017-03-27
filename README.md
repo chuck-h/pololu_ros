d936c15bbcf85df9daa71008b1f5a076c242b816
machewil <mattwilsonmbw@gmail.com>
2017-03-26 19:22:56 -0600

    adding functionality for standard config

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # pololu_ros
-ROS driver for Pololu Simple Motor Controller
+
+This is a ROS driver for the Pololu Simple Motor Controller.  It uses the
+Pololu Protocol to communicate over serial to control a motor.
+
+For a project I am working on, we are using the daisy chain configuration.
+If you want standard behavior, you will have to rewrite the Daisy chain so that
+multiple objects don't share a port.  I am planning on doing that later because
+it would be easy. If you are interested in this, raise an issue and I can work
+on that.  Or you can submit a pull request.
--- src/pololu_driver.py
+++ src/pololu_driver.py
@@ -6,43 +6,72 @@ __email__ = 'mattwilsonmbw@gmail.com'
 
 PROTOCOL = chr(0xAA) # First part of command to write using Pololu Protocol
 FORWARD = chr(0x05) # Drive motor forward command
-START = chr(0x03) # Exit Safe-Start mode so the motor can run 
-STOP = chr(0x60)  # Stops the motor and enters Safe-Start mode  
+START = chr(0x03) # Exit Safe-Start mode so the motor can run
+STOP = chr(0x60)  # Stops the motor and enters Safe-Start mode
 
 
-class Daisy:
+class Pololu:
+    """Represents a single Pololu Simple Motor Controller in standard config""""
+    def __init__(self, port="ttyACM0"):
+	"""Set motor id and open serial connection if not already open"""
+	self.dev_num = chr(dev_num)
+	if (Daisy.ser is None or not Pololu.ser.isOpen()):
+	    Daisy.ser = serial.Serial(port)
+
+    def __del__(self):
+	"""Decrement count and close port if it's the last connection"""
+	Daisy.count -= 1 # decrement running total of Pololu objects
+	if (Daisy.count <= 0):
+	    if (Daisy.ser is not None and Daisy.ser.isOpen()):
+		Daisy.ser.close()
+
+   def _send_command(command, databyte3, databyte4):
+        """Sends a two-byte command using the Pololu protocol."""
+	cmd = PROTOCOL + self.dev_num + command + chr(databyte3) + chr(databyte4)
+	Daisy.ser.write(cmd)
+
+    def _send_command_single(command):
+        """Sends a one-byte command using the Pololu protocol."""
+	cmd = PROTOCOL + self.dev_num + command
+	Daisy.ser.write(cmd)
+
+    def _exit_safe_start():
+	       self._send_command_single(START)
+    def _stop_motor():
+	       self._send_command_single(STOP)
+
+
+
+class Daisy(Pololu):
     """Represents a single Pololu Simple Motor Controller in daisy chain
-    
+
     This class offers an interface to daisy chain several Pololu Simple
     Motor Controllers. The daisy chained modules all share the same
-    serial connection.  To target specific devices, every command 
+    serial connection.  To target specific devices, every command
     is sent with the device number of the device.
 
-
-    Initialize every separate board you want to talk to with its 
+    Initialize every separate board you want to talk to with its
     device number.
 
     Example:
 	Here is an example of usage
         ::
-            pololu = Daisy(0) # initialize a Pololu with device number 0
-    
-    Todo:
-        * For module TODOs
-        * You have to also use ``sphinx.ext.todo`` extension
-    
+            p0 = Daisy(0) # initialize a Pololu daisy chained with device number 0
+            p1 = Daisy(1) # initialize a Pololu daisy chained with device number 1
+
+
     .. _Github Repo
-       http://github.com/matwilso
-    
+       http://github.com/matwilso/pololu_ros
+
     """
-    
+
     count = 0 # static variable to keep track of number of Daisys open
-    ser = None # initialize static variable ser to None  
-    
+    ser = None # initialize static variable ser to None
+
     def __init__(self, dev_num, port="ttyACM0"):
 	"""Set motor id and open serial connection if not already open"""
 	if (dev_num is not int):
-	    raise Exception("Invalid motor id, must set to id of motor for daisy chaining") 
+	    raise Exception("Invalid motor id, must set to id of motor for daisy chaining")
 
 	Daisy.count += 1
 	self.dev_num = chr(dev_num)
@@ -54,24 +83,17 @@ class Daisy:
 	Daisy.count -= 1 # decrement running total of Pololu objects
 	if (Daisy.count <= 0):
 	    if (Daisy.ser is not None and Daisy.ser.isOpen()):
-		Daisy.ser.close() 
+		Daisy.ser.close()
 
    def _send_command(command, databyte3, databyte4):
         """Sends a two-byte command using the Pololu protocol."""
 	cmd = PROTOCOL + self.dev_num + command + chr(databyte3) + chr(databyte4)
 	Daisy.ser.write(cmd)
-    
+
     def _send_command_single(command):
         """Sends a one-byte command using the Pololu protocol."""
 	cmd = PROTOCOL + self.dev_num + command
 	Daisy.ser.write(cmd)
 
-    def _exit_safe_start():
-	self._send_command_single(START) 
-    def _stop_motor():
-	self._send_command_single(STOP)
-    
     """ USER METHODS """
         # TODO
-
-
