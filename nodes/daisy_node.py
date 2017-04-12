004973ef578cdc8a342e85532441c6cde88ff49a
URMP Jetson <urmput@gmail.com>
2017-04-11 21:33:09 -0600

    fixed launch file port param and now only display the watchdog message once

--- nodes/daisy_node.py
+++ nodes/daisy_node.py
@@ -16,14 +16,9 @@ class Node(object):
         rospy.loginfo("Connecting to pololu daisy chain")
         self.last_set_speed_time = rospy.get_rostime()
         self.port = rospy.get_param("/emcee/daisy/port", "/dev/ttyUSB0") # param, default
-        # Subscribers
-        self.arm_sub = rospy.Subscriber("/emcee/arm/vel", Float32, self.arm_vel_callback, queue_size=1)
-        self.bucket_sub = rospy.Subscriber("/emcee/bucket/vel", Float32, self.bucket_vel_callback, queue_size=1)
-        self.pinion_sub = rospy.Subscriber("/emcee/pinion/vel", Float32, self.pinion_vel_callback, queue_size=1)
 
         self.TIMEOUT = 2  # time between hearing commands before we shut off the motors
 
-        # rospy.sleep(1)
         rospy.logdebug("Daisy chain port: %s", self.port)
         rospy.logdebug("DAISY NODE TIMEOUT = %s", self.TIMEOUT)
 
@@ -32,14 +27,25 @@ class Node(object):
         self.arm_right_devnum = rospy.get_param("/emcee/daisy/linear_actuators/arm_right", "1")
         self.bucket_left_devnum = rospy.get_param("/emcee/daisy/linear_actuators/bucket_left", "2")
         self.bucket_right_devnum = rospy.get_param("/emcee/daisy/linear_actuators/bucket_right", "3")
-        self.pinion_devnum = rospy.get_param("/emcee/daisy/pinion", "4")
+        self.pinion_left_devnum = rospy.get_param("/emcee/daisy/pinions/left", "4")
+        self.pinion_right_devnum = rospy.get_param("/emcee/daisy/pinions/right", "5")
 
         # initalize Daisy chain serial controllers
         self.arm_left = Daisy(self.arm_left_devnum, port=self.port) # first one initialized must set the port
         self.arm_right = Daisy(self.arm_right_devnum)
         self.bucket_left = Daisy(self.bucket_left_devnum)
         self.bucket_right = Daisy(self.bucket_right_devnum)
-        self.pinion = Daisy(self.pinion_devnum)
+        self.pinion_left = Daisy(self.pinion_left_devnum)
+        self.pinion_right = Daisy(self.pinion_right_devnum)
+
+        # Subscribers
+        self.arm_sub = rospy.Subscriber("/emcee/arm/vel", Float32, self.arm_vel_callback, queue_size=1)
+        self.bucket_sub = rospy.Subscriber("/emcee/bucket/vel", Float32, self.bucket_vel_callback, queue_size=1)
+        self.pinion_sub = rospy.Subscriber("/emcee/pinion/vel", Float32, self.pinion_vel_callback, queue_size=1)
+
+        self._has_showed_message = False  # flag to indicate we have showed the motor shutdown message
+
+        #rospy.sleep(1)
 
     def _stop_all_motors(self):
         """Send stop command to all daisy chained motors"""
@@ -47,7 +53,8 @@ class Node(object):
         self.arm_right.stop()
         self.bucket_left.stop()
         self.bucket_right.stop()
-        self.pinion.stop()
+        self.pinion_left.stop()
+        self.pinion_right.stop()
 
     def run(self):
 	"""Run the main ros loop"""
@@ -58,7 +65,11 @@ class Node(object):
             # TODO: consider making individual ones for each of the controllers
             if (rospy.get_rostime() - self.last_set_speed_time).to_sec() > self.TIMEOUT:
                 self._stop_all_motors()
-                rospy.loginfo("No commands received in the last %d seconds. Shutting down motors",self.TIMEOUT)
+                if (not self._has_showed_message):
+                     rospy.loginfo("No commands received in the last %d seconds. SHUTTING DOWN MOTORS",self.TIMEOUT)
+                self._has_showed_message = True
+            else:
+                self._has_showed_message = False
 
             r_time.sleep()
 
@@ -99,7 +110,7 @@ class Node(object):
 	"""Command the rack and pinion based on the incoming command"""
         self.last_set_speed_time = rospy.get_rostime()
 
-        rospy.logdebug("Velocity command to pinion motor: %.4f", command.data)
+        rospy.logdebug("Velocity command to pinion motors: %.4f", command.data)
 
         motor_command = max(-1.0, min(command.data, 1.0)) # put bounds on the incoming command
         motor_command = int(command.data * 3200) # scale to that expected by drivers
@@ -107,8 +118,8 @@ class Node(object):
         rospy.logdebug("Pinion motor serial command = %d", motor_command)
 
         # drive both linear actuators with the same command
-        self.bucket_left.drive(motor_command)
-        self.bucket_left.drive(motor_command)
+        self.pinion_left.drive(motor_command)
+        self.pinion_right.drive(motor_command)
 
 
 
